35f85a31d816eb5cc8003d2679e9598649429155
parasop <makvanaparas564@gmail.com>
2022-08-20 11:24:23 +0000

    fixed

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # applemusic.js
-applemusic.js is a powerful Node.js  module that allows you to interact with the Apple Music API  very easily
+
+applemusic.js is a powerful Node.js module that allows you to interact with the Apple Music API very easily
+
+#### Installation
+
+```
+npm intall applemusic.js
+```
+
+## Usage
+```js
+const AppleMusic = require("applemusic.js");
+
+let apple = new AppleMusic()
+
+
+//examples
+
+let playlist = await apple.search("https://music.apple.com/us/album/bollywood-lofi/1610407639")
+
+let song = await apple.search("tum hi aana");
+
+
+
+```
+
+## [Donate us](https://ko-fi.com/parasdev)
+
+
+## [Discord Server](https://discord.gg/ghysw8CPBf)
---
+++ src/AppleMusic.js
@@ -0,0 +1,102 @@
+"use strict";
+const { fetch } = require("undici");
+let baseURL =
+  /(?:https:\/\/music\.apple\.com\/)(?:.+)?(artist|album|music-video|playlist)\/([\w\-\.]+(\/)+[\w\-\.]+|[^&]+)\/([\w\-\.]+(\/)+[\w\-\.]+|[^&]+)/;
+
+class AppleMusic {
+  constructor(options = {}) {
+    this.url = `https://amp-api.music.apple.com/v1/catalog/${
+      options.searchMarket || "IN"
+    }`;
+    this.token = null;
+    this.getToken();
+  }
+
+  check(url) {
+    return baseURL.test(url);
+  }
+
+  async getToken() {
+    try {
+      let req = await fetch("https://music.apple.com/us/browse");
+      let json = await req.text();
+      let config =
+        /<meta name="desktop-music-app\/config\/environment" content="(.*?)">/.exec(
+          json
+        );
+
+      let key = (config = JSON.parse(decodeURIComponent(config[1])));
+      let { token } = key?.MEDIA_API;
+
+      if (!token) throw new Error("No acess key found for apple music");
+
+      this.token = `Bearer ${token}`;
+    } catch (e) {
+      if (e.status === 400) {
+        throw new Error(`[Poru Apple Music]:${e}`);
+      }
+    }
+  }
+
+  async getData(param) {
+    if (!this.token) await this.getToken();
+
+    let req = await fetch(`${this.url}${param}`, {
+      headers: {
+        Authorization: `${this.token}`,
+        origin: "https://music.apple.com",
+      },
+    });
+
+    let body = await req.json();
+
+    return body;
+  }
+
+  async search(query) {
+    let [, type] = baseURL.exec(query) ?? [];
+
+    switch (type) {
+      case "playlist": {
+        return this.getPlaylist(query);
+      }
+      case "album": {
+        return this.getAlbum(query);
+      }
+      case "artist": {
+        return this.getArtist(query);
+      }
+      default: {
+        return this.getSongByWords(query);
+      }
+    }
+  }
+
+  async getSongByWords(query) {
+    let tracks = await this.getData(`/search?types=songs&term=${query}`);
+    return tracks;
+  }
+
+  async getPlaylist(url) {
+    let query = new URL(url).pathname.split("/");
+    let id = query.pop();
+    let playlist = await this.getData(`/playlists/${id}`);
+    return playlist;
+  }
+
+  async getAlbum(url) {
+    let query = new URL(url).pathname.split("/");
+    let id = query.pop();
+    let album = await this.getData(`/albums/${id}`);
+    return album;
+  }
+
+  async geArtist(url) {
+    let query = new URL(url).pathname.split("/");
+    let id = query.pop();
+    let artist = await this.getData(`/attists/${id}`);
+    return artist;
+  }
+}
+
+module.exports = AppleMusic;
